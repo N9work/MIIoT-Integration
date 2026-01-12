e944d76872824ce63772fd999a924894f0c47d91
N9work <nicekrub2546@gmail.com>
2026-01-12 09:45:53 +0700

    change file name from db.js to postgresdb.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const db = require('./config/db');
+const db = require('./config/postgresdb');
 
 // เรียกใช้ Route
 const authRoutes = require('./routes/authRoutes'); 
